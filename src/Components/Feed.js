9af9aae7c3769f1e242a3dd52e91e89ab2a60bc6
Ryusei Best <ryusei10best@gmail.com>
2022-11-11 19:00:14 -0800

    first

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -6,13 +6,15 @@ import Block from './Block';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
+    const [blocks, setBlocks] = useState([]);
 
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const posts = blocks.map((color) => 
+    <Block color = {color} caption = {color}/>); // TODO: edit this variable
 
     return (
         <div>
-            <Menu></Menu>
+            <Menu handleClick={(color) => setBlocks(blocks => [color, ...blocks])}></Menu>
 
             {/* Below is where all of your Blocks should render! */}
             {posts}
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,15 @@
 import React from 'react';
 /* Add any imports you think you might need here! */
+import Color from './Color';
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color="red" handleClick={props.handleClick}></Color>
+          <Color color="blue" handleClick={props.handleClick}></Color>
+          <Color color="yellow" handleClick={props.handleClick}></Color>
+          <Color color="green" handleClick={props.handleClick}></Color>
       </div>
     );
 }
